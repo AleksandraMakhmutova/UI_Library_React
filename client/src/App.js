56b61542cf599ec3393ff05be0657a2a1cecb8e9
HoneyMS <Honeyms@mail.ru>
2021-02-25 16:07:16 +0300

    create nav with stycky position

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
 
+import { Header } from './Component/Header';
+import './App.css'
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <Header/>
   );
 }
 
--- client/src/Component/Header/index.js
+++ client/src/Component/Header/index.js
@@ -1,20 +1,38 @@
-import React from "react"
-import style from "./style,module.css"
-import cn from 'classnames'
+import React from "react";
+import "./index.css";
+
+
+
 export function Header(){
+
 	return (
-		<header className={cn(style.header, style.outer)}>
-  <div className={cn(style.header, style.inner, style.responsive, style.wrapper)}>
-		<div className={cn(style.header, style.logo)}><img src="https://assets.codepen.io/285131/acme-2.svg" /></div>
-		<nav className={cn(style.header, style.navigation)}>
-		  <a href="#">Home</a>
+		<>
+
+<header className="header-outer">
+  <div className="header-inner responsive-wrapper">
+    <div className="header-logo">
+      <img src="https://assets.codepen.io/285131/acme-2.svg" />
+    </div>
+    <nav className="header-navigation">
+      <a href="#">Home</a>
       <a href="#">About</a>
       <a href="#">Blog</a>
       <a href="#">Contact Us</a>
       <button>Menu</button>
-		</nav>
-	</div>
-			
-		</header>
+    </nav>
+  </div>
+</header>
+<main className="main">
+  <div className="main-content responsive-wrapper">
+    <article className="widget">
+      <h2>How does it work without JS? 🤯</h2>
+      <p>This sticky header consists of two elements: an <strong>outer</strong> and an <strong>inner</strong> container. The outer container is taller than the inner — and the inner is centered vertically.</p>
+      <p>By utilizing the <code>position: sticky</code> property <strong>twice</strong>, both on the header's <strong>outer container</strong> and <strong>inner container</strong> the outer container will stick to the <code>body</code>, while the inner container will stick to the outer container.</p>
+      <p>Note that the outer container has a negative <code>top</code> value equal to the height difference between the two containers. This causes the outer container to stick <strong>above</strong> the <code>body</code>, making the inner container stick to the "ceiling" on scroll.
+      </p>
+    </article>
+  </div>
+</main>
+</>
 	)
 }
